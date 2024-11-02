14cb9b58680ec48d70e1a7d94f5423e186a47b7e
Abdelouahed06 <kaabwd@gmail.com>
2024-11-02 17:17:06 +0100

    update

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,5 +1,24 @@
 public class Main {
+    public static long toMilesPerHour(double kilometersPerHour) {
+        if (kilometersPerHour < 0) {
+            return -1;
+        }
+        return Math.round(kilometersPerHour / 1.609);
+    }
+
+    public static void printConversion(double kilometersPerHour) {
+        long milesPerHour = toMilesPerHour(kilometersPerHour);
+        System.out.println("hna ------> " + milesPerHour);
+
+        if (milesPerHour == -1) {
+            System.out.println("Invalid Value");
+        } else {
+            System.out.println(kilometersPerHour + " km/h = " + milesPerHour + " mi/h");
+        }
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        printConversion(1.5); // You can change this value to test with different inputs
     }
 }
