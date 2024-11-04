55526e60d7512e890367e2191937422d2e501ff5
Abdelouahed06 <kaabwd@gmail.com>
2024-11-04 20:56:00 +0100

    update ex01

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+//import java.util.Scanner;
 public class Main {
 //    public static long toMilesPerHour(double kilometersPerHour) {
 //        if (kilometersPerHour < 0) {
